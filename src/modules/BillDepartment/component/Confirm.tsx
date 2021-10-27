a0219dfe0f9a858c99fe22437804620de9c3ea30
CuongDC <ducuong2017@gmail.com>
2021-10-27 16:18:04 +0700

    update

--- src/modules/BillDepartment/component/Confirm.tsx
+++ src/modules/BillDepartment/component/Confirm.tsx
@@ -2,13 +2,14 @@ import { Input, Modal } from "antd";
 import { observer } from "mobx-react";
 import React from "react";
 import { control } from "../store";
+import { Button } from 'antd';
 @observer
 export default class Confirm extends React.Component {
     render() {
         return (
             <Modal zIndex={100} onCancel={() => control.store.isConfirm = false} onOk={() => control.confirmFees()} okText={"Xác nhận"} cancelText={"Huỷ"} visible={control.store.isConfirm}>
                 <Input onChange={(e) => control.store.otp = e.target.value} />
-                <button onClick={() => control.sendOtp("pc")}>Gửi lại mã xác nhận</button>
+                <Button onClick={() => control.sendOtp("pc")}>Gửi lại mã xác nhận</Button>
             </Modal>
         )
     }
--- src/modules/accountant/component/Confirm.tsx
+++ src/modules/accountant/component/Confirm.tsx
@@ -2,13 +2,14 @@ import { Modal, Input } from "antd";
 import { observer } from "mobx-react";
 import React from "react";
 import { control } from "../../BillDepartment/store";
+import { Button } from 'antd';
 @observer
 export default class Confirm extends React.Component {
     render() {
         return (
             <Modal zIndex={100} title={"Vui lòng nhập otp"} onCancel={() => control.store.isConfirm = false} onOk={() => control.confirmAccountant()} okText={"Xác nhận"} cancelText={"Huỷ"} visible={control.store.isConfirm}>
                 <Input onChange={(e) => control.store.otp = e.target.value} />
-                <button onClick={() => control.sendOtp("kt")}>Gửi lại mã xác nhận</button>
+                <Button type="link" onClick={() => control.sendOtp("kt")}>Gửi lại mã xác nhận</Button>
             </Modal>
         )
     }
