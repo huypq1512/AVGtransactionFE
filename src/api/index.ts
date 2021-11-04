aec1249d8870ec0fc9274f2a1b032ea2f38c1fa6
huypq1512 <huypham1512@gmail.com>
2021-11-04 07:26:02 +0000

    edit api/index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ export interface IApiResponse<T> {
     status: number
     body: T
 }
-export const baseURLFE: string = "http://localhost:3002/"
+export const baseURLFE: string = "http://hotro.avg.vn:4444/"
 export const baseURL: string = process.env.REACT_APP_API_BASE_URL || "";
 const TOKEN_NAME: string = "x-access-token";
 function getRequest(url: string, isToken: boolean = true): Promise<IApiResponse<any>> {
